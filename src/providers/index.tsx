dcaa973a3ec3691518aa882fc094dd3c8aaff83d
Ilya Bondarenko <bondarenkoilya2@gmail.com>
2025-04-05 01:09:53 +0300

    add theme

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,6 +1,12 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "../router";
+import { ThemeProvider } from "@emotion/react";
+import { defaultTheme } from "../themes";
 
 export const Providers = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
 };
