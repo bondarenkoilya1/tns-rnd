fc5ab21d94578691ae43079f3b8371ec15879ea2
Ilya Bondarenko <bondarenkoilya2@gmail.com>
2025-04-04 21:35:26 +0300

    install MUI and add a few main buttons to main page

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import { App } from "../App.tsx";
+import { Main } from "../pages/Main";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />
+    element: <App />,
+    children: [{ index: true, element: <Main /> }]
   }
 ]);
