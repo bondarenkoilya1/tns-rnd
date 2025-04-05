17fcb32c66e73b709b9de5801d698ba29db0d358
Ilya Bondarenko <bondarenkoilya2@gmail.com>
2025-04-05 23:09:08 +0300

    change discussion store, change error messages

--- src/pages/Cabinet/Cabinet.tsx
+++ src/pages/Cabinet/Cabinet.tsx
@@ -1,3 +1,34 @@
+import { ContainerStyled } from "../../../styled";
+import { Discussion } from "../../components/ui/Discussion";
+import { v4 as uuidv4 } from "uuid";
+import { useDiscussionsStore } from "../../store";
+import { useEffect } from "react";
+
 export const Cabinet = () => {
-  return <>Личный кабинет</>;
+  const { discussions, setDiscussions } = useDiscussionsStore();
+
+  useEffect(() => {
+    for (let i = 0; i < 3; i++)
+      setDiscussions({
+        id: uuidv4(),
+        title: `Опрос #${i}`,
+        description: `Описание #${i} опроса`,
+        votingType: "online",
+        endTime: "сегодня"
+      });
+  }, []);
+
+  return (
+    <ContainerStyled>
+      {discussions.map(({ id, title, description, votingType, endTime }) => (
+        <Discussion
+          key={id}
+          title={title}
+          description={description}
+          votingType={votingType}
+          endTime={endTime}
+        />
+      ))}
+    </ContainerStyled>
+  );
 };
