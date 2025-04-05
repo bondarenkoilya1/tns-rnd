13b62be38cf24bf9c13e425763af34d681b47031
Ilya Bondarenko <bondarenkoilya2@gmail.com>
2025-04-05 17:26:33 +0300

    apply new styles for header, video and text chats, set new app name

---
+++ src/components/VideoChat/styled.ts
@@ -0,0 +1,19 @@
+import styled from "@emotion/styled";
+
+export const VideoChatStyled = styled.section`
+  padding-top: 40px;
+`;
+
+export const VideoChatTitleStyled = styled.h1`
+  font-size: 36px;
+
+  & span {
+    color: #4848f4;
+  }
+`;
+
+export const VideoChatInputsContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
