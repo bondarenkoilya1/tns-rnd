6d81e2e70cdf7869469b025139f4d562478d3fbf
Ilya Bondarenko <bondarenkoilya2@gmail.com>
2025-04-06 16:07:05 +0300

    hard-core env variable in a rush

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,4 @@
 export const USER_API_URL: string = import.meta.env.VITE_USER_API_URL;
 export const VIDEO_CHAT_API_URL: string = import.meta.env.VITE_VIDEO_CHAT_API_URL;
-export const TEMPORARY_ACCESS_TOKEN: string = import.meta.env.VITE_TEMPORARY_ACCESS_TOKEN;
+export const TEMPORARY_ACCESS_TOKEN: string =
+  import.meta.env.VITE_TEMPORARY_ACCESS_TOKEN || "bc78256d93ae94a54fb8903a2a8ab2bbd3c1169a";
